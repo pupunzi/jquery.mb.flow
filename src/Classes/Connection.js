f3601c8b4c14cb74294c4f84134470a343598b2b
Matteo Bicocchi <matbicoc@gmail.com>
2021-05-13 19:47:57 +0200

    no message

--- src/Classes/Connection.js
+++ src/Classes/Connection.js
@@ -6,11 +6,12 @@
 import {Util} from "./Util.js";
 
 export class Connection {
-	constructor(connectionLine, fromNodeId, fromNodeElementId, toNodeId) {
+	constructor(connectionLine, fromNodeId, fromNodeElementId, toNodeId, type = 0) {
 		this._id = Util.setUID();
 		this._connectionLine = connectionLine;
 		this._from = fromNodeId;
 		this._nodeElementId = fromNodeElementId;
 		this._to = toNodeId;
+		this._type = type;
 	}
 }
